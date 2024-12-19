45bbad97feffe0cad0d2213d18fd359daae59397
Lanyer02 <odlanyernobela152@gmail.com>
2024-12-19 19:54:56 +0800

    animation

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -36,16 +36,16 @@ const SocialLinks = () => {
   ];
 
   return (
-    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed duration-300'>
+    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
       <ul>
         {links.map(({ id, child, href, style }) => (
           <li
             key={id}
-            className={`flex justify-between items-center w-40 h-14 px-4 bg-white ml-[-100px] hover:ml-[-10px] duration-300 transition` + (style ? ` ${style}` : '')}
+            className={`flex justify-between items-center w-40 h-14 px-4 bg-white ml-[-100px] hover:ml-[-10px] duration-300 ease-in-out transition-all` + (style ? ` ${style}` : '')}
           >
             <a
               href={href}
-              className='flex justify-between items-center w-full text-black duration-300'
+              className='flex justify-between items-center w-full text-black duration-300 ease-in-out'
               target='_blank'
               rel='noreferrer' 
             >
